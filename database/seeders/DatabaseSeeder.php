115c1996cb21ce4e33a8a5399d74019887df9c77
Carlos Eduardo Campos Rodrigues <Carlos.admcode@gmail.com>
2025-01-23 10:58:16 -0300

    Criando Migrate, Model e Controlle das tabelas Produtos e Categorias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Categoria;
+use App\Models\Produto;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            //UsersSeeder::class,
+            //CategoriaSeeder::class,
+            ProdutoSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/usersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+use function Laravel\Prompts\password;
+
+class usersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'firstName' => 'Rodrigo',
+            'lastName' => 'Oliveira',
+            'email'    => 'contato@rodrigo.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
